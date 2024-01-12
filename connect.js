93a55e1f047d012cd644846bfddd12b2604d51e9
vaibhav-1-kaushik <147250770+vaibhav-1-kaushik@users.noreply.github.com>
2024-01-12 19:01:08 +0530

    Add files via upload

---
+++ connect.js
@@ -0,0 +1,8 @@
+function Start(){
+    document.getElementById("Start").style.display="none"
+    document.getElementById("rr").style.display="flex"
+    
+  
+    document.getElementById("tt").innerHTML=Math.floor(Math.random() * 1000000000);
+
+}
